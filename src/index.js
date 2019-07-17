db5bf0d9e204480885c7ddc47504fb5b2e31627c
freedomzzzii <theeraporn.tia@gmail.com>
2019-07-18 01:14:23 +0700

    [update] add store to root app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import dotenv from 'dotenv';
 
+import configureStore from './configureStore';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+dotenv.load();
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
