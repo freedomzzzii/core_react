51f59f5883294716ab2dac1b3ed27638d04663ac
freedomzzzii <theeraporn.tia@gmail.com>
2019-07-18 01:12:45 +0700

    [create] entry point action

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import {
+  GET_POSTS_SUCCESS,
+  GET_POSTS_FAILURE,
+  fetchGetPosts,
+} from './Sample';
+
+export {
+  // sample
+  GET_POSTS_SUCCESS,
+  GET_POSTS_FAILURE,
+  fetchGetPosts,
+};
