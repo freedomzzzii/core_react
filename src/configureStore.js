d3bf36d1bab84ec2145832b828f814489aeebbe0
freedomzzzii <theeraporn.tia@gmail.com>
2020-09-15 17:36:45 +0700

    [create] config store

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,21 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
-// import thunkMiddleware from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import { createLogger } from 'redux-logger';
 
 import rootReducer from './reducers';
-import rootSaga from './saga';
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
-
-sagaMiddleware.run(rootSaga);
 
 if (process.env.NODE_ENV !== 'production') {
   middleware.push(createLogger());
 }
 
-export default preloadedState => (
+const store = preloadedState => (
   createStore(
     rootReducer,
     preloadedState,
@@ -23,4 +19,7 @@ export default preloadedState => (
   )
 );
 
-
+export {
+  store,
+  sagaMiddleware,
+};
