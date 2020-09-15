263f036a8227ba10bd70cc0022d72f42c417f2a1
freedomzzzii <theeraporn.tia@gmail.com>
2020-09-15 17:29:08 +0700

    [create] reducer

--- src/reducers/Language/index.js
+++ src/reducers/Language/index.js
@@ -1,7 +1,6 @@
 import commonConstant from '../../common/commonConstant';
 
 export const language = (state, action) => {
-  console.log('language>>>');
   switch (action.type) {
     case commonConstant.SET_LANGUAGE:
       return Object.assign({}, state, { ...action });
